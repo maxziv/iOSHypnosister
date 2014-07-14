6b208e466ea85150b819ad8c61b3a369c1dc83f0
Maxwell Zhou <mazhou@mazhou-mn2.linkedin.biz>
2014-07-14 11:21:58 -0700

    Initial Commit

---
+++ Hypnosister/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Hypnosister
+//
+//  Created by Maxwell Zhou on 7/14/14.
+//  Copyright (c) 2014 LinkedIn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
