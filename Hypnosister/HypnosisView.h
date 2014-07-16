b3d5fa03abbc7f807e26cda22fc3ad92c34e5104
Maxwell Zhou <yale.developer@gmail.com>
2014-07-15 23:37:32 -0700

    first commit

---
+++ Hypnosister/HypnosisView.h
@@ -0,0 +1,13 @@
+//
+//  HypnosisView.h
+//  Hypnosister
+//
+//  Created by Maxwell Zhou on 7/14/14.
+//  Copyright (c) 2014 LinkedIn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnosisView : UIView
+
+@end
---
+++ Hypnosister/HypnosisView.m
@@ -0,0 +1,31 @@
+//
+//  HypnosisView.m
+//  Hypnosister
+//
+//  Created by Maxwell Zhou on 7/14/14.
+//  Copyright (c) 2014 LinkedIn. All rights reserved.
+//
+
+#import "HypnosisView.h"
+
+@implementation HypnosisView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
